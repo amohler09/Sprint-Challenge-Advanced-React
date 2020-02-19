56039dbb0a4846a11b85caaa4120621e4b435279
Amber Mohler <ambermohler@iMac.local>
2020-02-19 16:08:59 -0500

    added custom hooks uselocalstorage and darkmode

--- client/src/PlayerCard.js
+++ client/src/PlayerCard.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export const PlayerCard = props => {
     return (
-        <div>
-            Hello
+        <div className='player-div'>
+            {props.players.map((item, index) => (
+                <div className='player-card' key={index}>
+                    <h3>{item.name}</h3>
+                    <p>{item.country}</p>
+                    <p>{item.searches}</p>
+
+                </div>
+            ))}
         </div>
     )
 }
